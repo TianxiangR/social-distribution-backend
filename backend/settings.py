c2b64a58e57c8dd29afc7c6d0f3dfba72286a01d
TianxiangR <ren644120622@gmail.com>
2023-11-22 16:32:37 -0700

    fix: switch from sqlite3 to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -100,12 +100,24 @@ AUTH_USER_MODEL = 'api.User'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('DB_PASSWORD', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd2ujniac4banhk',
+            'USER': 'eworqyeseybmwt',
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': 'ec2-44-213-228-107.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
