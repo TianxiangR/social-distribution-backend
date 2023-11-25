0156b9002e9a3324d2af6720a944d01037fdc132
TianxiangR <ren644120622@gmail.com>
2023-11-25 02:36:30 -0700

    fix: update author detail url

--- api/urls/remote_urls.py
+++ api/urls/remote_urls.py
@@ -5,7 +5,7 @@ from ..views import authors, posts, comments, followers, inbox
 urlpatterns = [
     # cross site apis
     path('authors/', authors.AuthorListRemote.as_view() , name='author-list'),
-    path('authors/<uuid:author_id>/', authors.AuthorDetail.as_view(), name='author-detail'),
+    path('authors/<uuid:author_id>', authors.AuthorDetail.as_view(), name='author-detail'),
     path('authors/<uuid:author_id>/posts/', posts.PostListRemote.as_view(), name='post-list'),
     path('authors/<uuid:author_id>/posts/<uuid:post_id>', posts.PostDetailRemote.as_view(), name='post-detail'),
     path('authors/<uuid:author_id>/posts/<uuid:post_id>/comments', comments.CommentListRemote.as_view(), name='comment-list'),
