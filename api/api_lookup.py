a38705aa3010b479828900363897a0c5ea50c65b
Tianxiang Ren <tianxia3@ualberta.ca>
2023-12-01 18:25:24 -0700

    feat: connect with RESTless Clients

--- api/api_lookup.py
+++ api/api_lookup.py
@@ -1,11 +1,14 @@
 from .server_adapters.I_am_teapot_adapter import IAmTeaPotAdapter
 from .server_adapters.ctrl_alt_defeat_adapter import CtrlAltDefeatAdapter
+from .server_adapters.restless_clients_adapter import RestLessClientsAdapter
 
 iAmTeaPotAdapter = IAmTeaPotAdapter()
 ctrlAltDefeatAdapter = CtrlAltDefeatAdapter()
+restLesClientsAdapter = RestLessClientsAdapter()
 
 API_LOOKUP = {
   iAmTeaPotAdapter.host: iAmTeaPotAdapter,
-  ctrlAltDefeatAdapter.host: ctrlAltDefeatAdapter
+  ctrlAltDefeatAdapter.host: ctrlAltDefeatAdapter,
+  # restLesClientsAdapter.host: restLesClientsAdapter
 }
 
